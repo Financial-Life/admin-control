ecc693e11d1b7f9c9a26ff007f347351025c1f6c
Ernesto García <ernt26dev@gmail.com>
2020-10-17 21:22:59 -0500

    Rename help to readme

--- src/main/java/com/hackathon/admin/AdminControlApplication.java
+++ src/main/java/com/hackathon/admin/AdminControlApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdminControlApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdminControlApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(AdminControlApplication.class, args);
+  }
 
 }
